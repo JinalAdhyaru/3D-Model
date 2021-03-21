bd9879fa0f949d89ca4f76ad86477d5b49266878
Jinal <adhyarujinal@gmail.com>
2021-03-21 20:47:48 +0530

    new changes

--- public/JS/model.js
+++ public/JS/model.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000);
+scene.background = new THREE.Color(0xD3D3D3);
 var camera = new THREE.PerspectiveCamera(30, (window.innerWidth)/(window.innerHeight), 50, 1000);
-camera.position.z = 5;
+camera.position.set(0,150,-300);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight);
@@ -24,14 +24,14 @@ var ambientLight = new THREE.AmbientLight(0xe3dac9); // soft white light
 scene.add(ambientLight);
 
 var pointLight = new THREE.PointLight(0xf5c693, 0.8);
-pointLight.position.set(0, 0, 70);
+pointLight.position.set(-10, 20, -70);
 scene.add(pointLight);
 
 var loader = new THREE.STLLoader();
 loader.load('STL/Shoulder_Bone.stl', function (geometry) {
-    var material = new THREE.MeshPhongMaterial({color: 0x696969});
+    var material = new THREE.MeshPhongMaterial({color: 0xA9A9A9});
     var bone = new THREE.Mesh(geometry, material);
-    bone.position.set(60, -70,100);
+    bone.position.set(50, -75,750);
     scene.add(bone);
 });
 
@@ -40,9 +40,9 @@ loader.load('STL/Shoulder_Bone.stl', function (geometry) {
     
 // };
 
-var gridXZ = new THREE.GridHelper(100, 100);
-gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
-scene.add(gridXZ);
+// var gridXZ = new THREE.GridHelper(100, 100);
+// gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
+// scene.add(gridXZ);
 
 
 // const axesHelper = new THREE.AxesHelper(3);
